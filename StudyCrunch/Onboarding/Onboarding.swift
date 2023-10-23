57720ae399ee17ecda7842bf4a4c05979fe3463f
Krakqn <139305417+Krakqn@users.noreply.github.com>
2023-10-23 00:39:33 -0700

    Original Commit.

---
+++ StudyCrunch/Onboarding/Onboarding.swift
@@ -0,0 +1,18 @@
+//
+//  Onboarding.swift
+//  StudyCrunch
+//
+//  Created by Sri Yanamandra on 10/23/23.
+//
+
+import SwiftUI
+
+private let BG_GRAD_DARK = Color.hex("FFB13D")
+private let BG_GRAD_LIGHT = Color.hex("FFCC02")
+
+func nextStep() {
+  withAnimation(.spring()) {
+    print("success")
+  }
+}
+
