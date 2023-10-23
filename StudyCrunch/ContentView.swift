87ca2cbbfd204bc9f775d48ce70f2a330d538ef9
Krakqn <139305417+Krakqn@users.noreply.github.com>
2023-10-23 00:03:27 -0700

    Initial Commit

---
+++ StudyCrunch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  StudyCrunch
+//
+//  Created by Sri Yanamandra on 10/23/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ StudyCrunch/StudyCrunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  StudyCrunchApp.swift
+//  StudyCrunch
+//
+//  Created by Sri Yanamandra on 10/23/23.
+//
+
+import SwiftUI
+
+@main
+struct StudyCrunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
