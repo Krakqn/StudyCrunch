c22272585ce97b7ac747f3335d31fa302e38c289
Ethan Kim <87978570+EthanKim8683@users.noreply.github.com>
2023-11-02 02:24:49 -0700

    Fix before applying stash

--- StudyCrunch/Global/constants.swift
+++
@@ -1,8 +0,0 @@
-//
-//  constants.swift
-//  StudyCrunch
-//
-//  Created by Ethan Kim on 10/26/23.
-//
-
-import Foundation
--- StudyCrunch/Global/global.swift
+++
@@ -1,8 +0,0 @@
-//
-//  global.swift
-//  StudyCrunch
-//
-//  Created by Ethan Kim on 10/26/23.
-//
-
-import Foundation
