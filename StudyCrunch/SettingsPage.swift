291bcf88bb62b5bfa12ea1a0d9a6d4a89e2eeb32
Ethan Kim <87978570+EthanKim8683@users.noreply.github.com>
2023-11-02 19:50:07 -0700

    trash

---
+++ StudyCrunch/SettingsPage.swift
@@ -0,0 +1,15 @@
+//
+//  Settings.swift
+//  StudyCrunch
+//
+//  Created by Ethan Kim on 10/26/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct SettingsPage: View {
+  var body: some View {
+    Text("Settings!")
+  }
+}
