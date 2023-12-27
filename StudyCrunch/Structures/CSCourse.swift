316f5bb52bfc586d726b4a17012230267fc5f5e2
Jun Gu <druidream@gmail.com>
2023-12-27 16:23:32 +0800

    manage chapters data in separate files

---
+++ StudyCrunch/Structures/CSCourse.swift
@@ -0,0 +1,50 @@
+//
+//  CSCourse.swift
+//  StudyCrunch
+//
+//  Created by Jun Gu on 12/27/23.
+//
+
+struct CSCourse {
+  let course: Course = {
+    try! Course.Builder()
+      .setEmoji(emoji: "💻")
+      .setName(name: "Computer Science")
+      .setDescription(description: "The best subject!")
+      .setChapterBuilders(chapterBuilders:[
+        Chapter.Builder()
+          .setName(name: "Variables")
+          .setDescription(description: "Super useful")
+          .setFlashcards(jsonFileName: "computer-science")
+          .setMarkdown(markdownFilename: "sample"),
+        Chapter.Builder()
+          .setName(name: "Loops")
+          .setMarkdown(markdownFilename: "sample"),
+        Chapter.Builder()
+          .setName(name: "Conditionals")
+          .setMarkdown(markdownFilename: "sample"),
+        Chapter.Builder()
+          .setName(name: "Arrays")
+          .setMarkdown(markdownFilename: "sample"),
+        Chapter.Builder()
+          .setName(name: "Pointers")
+          .setMarkdown(markdownFilename: "sample"),
+        Chapter.Builder()
+          .setName(name: "Complexity")
+          .setMarkdown(markdownFilename: "sample"),
+        Chapter.Builder()
+          .setName(name: "Depth-first search")
+          .setMarkdown(markdownFilename: "sample"),
+        Chapter.Builder()
+          .setName(name: "Complexity")
+          .setMarkdown(markdownFilename: "sample"),
+        Chapter.Builder()
+          .setName(name: "Depth-first search")
+          .setMarkdown(markdownFilename: "sample"),
+        Chapter.Builder()
+          .setName(name: "Depth-first search")
+          .setMarkdown(markdownFilename: "sample"),
+      ])
+      .build()
+  }()
+}
