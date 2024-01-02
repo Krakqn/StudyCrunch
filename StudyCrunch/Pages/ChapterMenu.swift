d5f8cd429de41eb3f7771ab3572ac85c20c3f89a
Krakqn <139305417+Krakqn@users.noreply.github.com>
2024-01-02 00:27:50 -0800

    Added the animations to ChapterMenu and SectionMenu as well.

--- StudyCrunch/Pages/ChapterMenu.swift
+++ StudyCrunch/Pages/ChapterMenu.swift
@@ -9,23 +9,38 @@ import Foundation
 import SwiftUI
 
 struct ChapterMenu: View {
-  var section: Section
-  
-  var body: some View {
-    NavigationStack {
-      ScrollView {
-        ForEach(section.chapters) { chapter in
-          NavigationLink {
-            ChapterPage(chapter: chapter, section: section)
-          } label: {
-            let accessoryIcon = chapter.restricted ? "lock.fill" : nil
-            MenuOption(symbol: chapter.symbol, name: chapter.name, description: chapter.description, accessoryIcon: accessoryIcon)
-              .padding(.horizontal)
-          }
+    var section: Section
+    @State private var isPressed: [UUID: Bool] = [:]
+
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                ForEach(section.chapters) { chapter in
+                    let chapterPressedState = Binding(
+                        get: { isPressed[chapter.id, default: false] },
+                        set: { isPressed[chapter.id] = $0 }
+                    )
+
+                    NavigationLink {
+                        ChapterPage(chapter: chapter, section: section)
+                    } label: {
+                        let accessoryIcon = chapter.restricted ? "lock.fill" : nil
+                        MenuOption(symbol: chapter.symbol, name: chapter.name, description: chapter.description, accessoryIcon: accessoryIcon)
+                            .padding(.horizontal)
+                    }
+                    ._onButtonGesture {
+                        chapterPressedState.wrappedValue = $0
+                    } perform: {
+
+                    }
+                    .conditionalEffect(.pushDown, condition: chapterPressedState.wrappedValue)
+                    .onAppear {
+                        isPressed[chapter.id] = false
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Chapters")
         }
-      }
-      .navigationBarTitleDisplayMode(.inline)
-      .navigationTitle("Chapters")
     }
-  }
 }
--- StudyCrunch/Pages/SectionMenu.swift
+++ StudyCrunch/Pages/SectionMenu.swift
@@ -7,24 +7,41 @@
 
 import Foundation
 import SwiftUI
+import Pow
 
 struct SectionMenu: View {
-  var course: Course
-  
-  var body: some View {
-    NavigationStack {
-      ScrollView {
-        ForEach(course.sections) { section in
-          NavigationLink {
-            ChapterMenu(section: section)
-          } label: {
-            MenuOption(symbol: section.symbol, name: section.name, description: section.description)
-              .padding(.horizontal)
-          }
+    var course: Course
+    @State private var isPressed: [UUID: Bool] = [:]
+
+    var body: some View {
+        NavigationStack {
+            ScrollView {
+                ForEach(course.sections) { section in
+                    let sectionPressedState = Binding(
+                        get: { isPressed[section.id, default: false] },
+                        set: { isPressed[section.id] = $0 }
+                    )
+
+                    NavigationLink {
+                        ChapterMenu(section: section)
+                    } label: {
+                        MenuOption(symbol: section.symbol, name: section.name, description: section.description)
+                            .padding(.horizontal)
+                    }
+                    ._onButtonGesture {
+                        sectionPressedState.wrappedValue = $0
+                    } perform: {
+
+                    }
+                    .conditionalEffect(.pushDown, condition: sectionPressedState.wrappedValue)
+                    .onAppear {
+                        isPressed[section.id] = false
+                    }
+                }
+            }
+            .navigationBarTitleDisplayMode(.inline)
+            .navigationTitle("Sections")
         }
-      }
-      .navigationBarTitleDisplayMode(.inline)
-      .navigationTitle("Sections")
     }
-  }
 }
+
